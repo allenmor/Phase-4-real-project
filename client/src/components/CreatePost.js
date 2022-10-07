9a6cde500b0566f76fd81233580bb35db4ef5b63
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-10-07 11:50:23 -0400

    create done

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import {Form, Button} from 'react-bootstrap';
 
 import './CreatePost.css'
 
@@ -36,17 +37,31 @@ function CreatePost({setCreateClicked, setSubmitClicked}) {
         navigate(`/profile`);
     }
 
-    // style={!createClicked ? {display: 'none'} : {display: 'block'}}
+ 
+       
   return (
     <div className='create-post-container'>
         <h2 className='h2-create-post'>Create Post</h2>
-        <form onSubmit={handleSubmit}>
-            <input className='post-input' onChange={handleChange} name='post_image' value={postObj.post_image} placeholder='Image' type='text'></input>
-            <input className='post-input' onChange={handleChange} name='description' value={postObj.description} placeholder='Description' type='text'></input>
-            <input className='submit-post' value='Submit' type='submit'></input>
-        </form>
+        <Form  onSubmit={handleSubmit} style={{ width: '35rem' }} className="text-center mt-4 mb-4">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label><h5>Post's Link</h5></Form.Label>
+                <Form.Control type="text"  placeholder='Image' name='post_image' value={postObj.post_image} onChange={handleChange}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label><h5>Your Post</h5></Form.Label>
+                <Form.Control as="textarea" rows={5} placeholder="Write you post here" value={postObj.description} name='description' onChange={handleChange}/>
+            </Form.Group>
+            <div className="col-md-12 text-center btn-group" style={{ width: '100%'}}>
+                <Button variant="dark" type="submit">
+                    Post
+                </Button>
+            </div>
+          
+        </Form> 
     </div>
   )
 }
 
 export default CreatePost;
+
+
